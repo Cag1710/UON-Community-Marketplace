1b181b4f54509e62412f59c9b923b2dab7684f0e
Cag1710 <cag1710@outlook.com>
2025-05-12 17:34:16 +1000

    Setup basic create account and login pages, setup a basic mainpage and nav bar, just so everyone has it immediately and can easily navigate between pages

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,14 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Layout from './Layout';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import CreateAccountPage from './pages/CreateAccountPage';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import NavBar from './NavBar';
+
+const routes = [{
+  path: '/',
+  element: <Layout />,
+  children: [{
+    path: '/',
+    element: <HomePage />
+  }, {
+    path: '/login',
+    element: <LoginPage />
+  }, {
+    path: '/create-account',
+    element: <CreateAccountPage />
+  }]
+}]
+
+
+const router = createBrowserRouter(routes);
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-        
+        <RouterProvider router={router} />
     </>
   )
 }
