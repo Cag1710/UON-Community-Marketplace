474a298a2a70e8f9480b14426229dcbf5eaad1c5
tylerhuntuon <61522278+tylerhuntuon@users.noreply.github.com>
2025-06-05 00:58:17 +1000

    Added description area of messaging section

--- front-end/src/components/Sending.jsx
+++ front-end/src/components/Sending.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ProfileIcon from "../assets/profile.svg"
 
 function Sending() {
     return(
@@ -14,18 +13,18 @@ function Sending() {
 
 const styles = {
     inputBox: {
-    padding: "10px",
-    width: "100%",
-    boxSizing: "border-box",
-  },
-  input: {
-    width: "95%",
-    padding: "10px",
-    fontSize: "16px",
-    borderRadius: "5px",
-    border: "1px solid #ccc",
-    marginBottom: "90px"
-  },
+        padding: "10px",
+        width: "100%",
+        boxSizing: "border-box",
+    },
+    input: {
+        width: "95%",
+        padding: "10px",
+        fontSize: "16px",
+        borderRadius: "5px",
+        border: "1px solid #ccc",
+        marginBottom: "90px"
+    },
 }
 
 export default Sending;
