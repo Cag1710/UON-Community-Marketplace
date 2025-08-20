33155ac78ff290e5052ab0fb568d612e46674e01
NoahCook12 <noah.cook1234567@gmail.com>
2025-08-21 09:23:18 +1000

    Update App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoggedOutHome from './pages/LoggedOutHome';
 import LoggedInHome from './pages/LoggedInHome'; 
+<<<<<<< Updated upstream
 import ListingsPage from './pages/ListingsPage';
+=======
+
+>>>>>>> Stashed changes
 
 
 function App() {
@@ -11,8 +15,11 @@ function App() {
       <Routes>
         <Route path="/" element={<LoggedOutHome />} />
         <Route path="/home" element={<LoggedInHome />} />
+<<<<<<< Updated upstream
         <Route path="/listings" element={<ListingsPage />} />
         <Route path="*" element={<div>404 - Not Found</div>} />
+=======
+>>>>>>> Stashed changes
       </Routes>
     </Router>
   );
